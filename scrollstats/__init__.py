a046c2bdb618f201c27a23588d596765917cf401
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-02-28 08:58:22 -0600

    updated ridge_metrics import

--- scrollstats/__init__.py
+++ scrollstats/__init__.py
@@ -7,5 +7,5 @@ This file is responsible for pulling the functions out of the modules
 
 # from utils import densify_line
 
-from .ridge_metrics import calculate_transect_metrics
+from .ridge_metrics import *
 from .utils import *
