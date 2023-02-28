cb9b78641afc07c07893567658d5725c2aab8108
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-02-27 23:35:08 -0600

    added calc_dist()

--- scrollstats/utils/__init__.py
+++ scrollstats/utils/__init__.py
@@ -1,3 +1,3 @@
-from .geom_utils import densify_line, transform_coords
+from .geom_utils import densify_line, transform_coords, calc_dist
 
-__all__ = ["densify_line", "transform_coords"]
+__all__ = ["densify_line", "transform_coords", "calc_dist"]
