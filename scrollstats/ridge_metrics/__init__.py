0ec794a9abd8572a1b2c24808f174bac15e51308
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-02-27 23:34:47 -0600

    Added amplitude funcs and __all__

--- scrollstats/ridge_metrics/__init__.py
+++ scrollstats/ridge_metrics/__init__.py
@@ -1,1 +1,6 @@
 from .createTransectMetrics import calculate_transect_metrics
+from .ridgeAmplitudes import calc_ridge_amps, map_amp_values
+
+__all__ = ["calculate_transect_metrics", 
+           "calc_ridge_amps",
+           "map_amp_values"]
