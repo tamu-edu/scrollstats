39fbb0f8accce07cfc460bac78d7cca8ac3a4bca
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-03-02 13:45:44 -0600

    added data extractors

--- scrollstats/ridge_metrics/__init__.py
+++ scrollstats/ridge_metrics/__init__.py
@@ -1,6 +1,10 @@
 from .createTransectMetrics import calculate_transect_metrics
 from .ridgeAmplitudes import calc_ridge_amps, map_amp_values
+from .data_extractors import RidgeDataExtractor, TransectDataExtractor, BendDataExtractor
 
 __all__ = ["calculate_transect_metrics", 
            "calc_ridge_amps",
-           "map_amp_values"]
+           "map_amp_values",
+           "RidgeDataExtractor", 
+           "TransectDataExtractor", 
+           "BendDataExtractor"]
