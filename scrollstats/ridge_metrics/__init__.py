3af2ea00257cd9c4f36acc4bd792b1dac317757d
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-03-04 22:56:23 -0600

    updated imports

--- scrollstats/ridge_metrics/__init__.py
+++ scrollstats/ridge_metrics/__init__.py
@@ -1,10 +1,11 @@
-from .createTransectMetrics import calculate_transect_metrics
+# from .createTransectMetrics import calculate_transect_metrics
 from .ridgeAmplitudes import calc_ridge_amps, map_amp_values
 from .data_extractors import RidgeDataExtractor, TransectDataExtractor, BendDataExtractor
+from .calc_ridge_metrics import calculate_ridge_metrics
 
-__all__ = ["calculate_transect_metrics", 
-           "calc_ridge_amps",
+__all__ = ["calc_ridge_amps",
            "map_amp_values",
            "RidgeDataExtractor", 
            "TransectDataExtractor", 
-           "BendDataExtractor"]
+           "BendDataExtractor",
+           "calculate_ridge_metrics"]
