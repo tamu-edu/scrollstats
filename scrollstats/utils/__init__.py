1b20fddbb1683891f75164c908c5c6b316ce1714
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-03-08 17:46:40 -0600

    added explode function

--- scrollstats/utils/__init__.py
+++ scrollstats/utils/__init__.py
@@ -1,3 +1,8 @@
-from .geom_utils import densify_line, transform_coords, calc_dist, meanfilt, calc_cubic_spline
+from .geom_utils import densify_line, transform_coords, calc_dist, meanfilt, calc_cubic_spline, explode
 
-__all__ = ["densify_line", "transform_coords", "calc_dist", "meanfilt", "calc_cubic_spline"]
+__all__ = ["densify_line", 
+           "transform_coords", 
+           "calc_dist",
+           "meanfilt",
+           "calc_cubic_spline",
+           "explode"]
