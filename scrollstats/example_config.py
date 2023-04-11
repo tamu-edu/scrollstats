e0986c592d828529721de3908777f70c61bf121e
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-04-11 11:01:10 -0500

    removed GLOBAL_PROJ_CRS in favor of gpd UTM method

--- scrollstats/example_config.py
+++ scrollstats/example_config.py
@@ -1,6 +1,8 @@
+__all__ = ["DB_PW",
+           "GLOBAL_GEOG_CRS"]
+
 # Password for DB
 DB_PW = "your_db_password"
 
 # Global-scale CRS used throughout the scripts
 GLOBAL_GEOG_CRS = "EPSG:4326"
-GLOBAL_PROJ_CRS = "ESRI:54009" 
