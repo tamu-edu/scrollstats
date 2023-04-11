47c487d710b4598a53b6b6615ac7ad3da1a4dda6
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-04-11 11:22:50 -0500

    formatting

--- scrollstats/__init__.py
+++ scrollstats/__init__.py
@@ -4,9 +4,6 @@ Development Note:
 This file is responsible for pulling the functions out of the modules 
 """
 
-
-# from utils import densify_line
-
 from .ridge_metrics import *
 from .utils import *
 from .transecting import *
