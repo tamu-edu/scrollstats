d40ae4f79a36d87e9836f316bd1522a6037d5405
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-09-22 12:20:30 -0500

    added constants

--- globals.py
+++ globals.py
@@ -1,5 +1,10 @@
-# Contains global variables such as file directories
+# Contains global variables such as file directories and other constants
 from pathlib import Path
 
 DATA_INPUT = Path("data/input")
 DATA_OUTPUT = Path("data/output")
+
+
+# Constants
+SMALL_FEATS_SIZE = 50      # Measured in img px
+ELONGATION_THRESHOLD = 80  # Percentage 
