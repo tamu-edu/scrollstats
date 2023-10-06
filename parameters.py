ab40f22254b57914bedb44a3e7ea46699c4d9447
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-10-06 11:46:06 -0500

    added and updated default parameters

--- parameters.py
+++ parameters.py
@@ -44,7 +44,17 @@ for RASTER_DIR in RASTER_DIRS.values():
         RASTER_DIR.mkdir(parents=True)
 
 
-# Constants
-WINDOW_SIZE = 45           # Measured in img px
+# Raster Processing Constants
+RASTER_WINDOW_SIZE = 45    # Measured in img px
 SMALL_FEATS_SIZE = 50      # Measured in img px
 ELONGATION_THRESHOLD = 80  # Percentage 
+
+# Vector Creation Constants
+SMOOTHING_WINDOW_SIZE = 5  # Measured in vertices
+VERTEX_SPACING = 1         # Measured in linear unit of dataset (meteres for example datasets)
+
+## See `CreatingVectorData.ipynb` for explanation of constants below and how they are used in migration pathway creation
+SHOOT_DISTANCE = 300       # Distance that the N1 coordinate will shoot out from point P1; easured in linear unit of dataset
+SEARCH_DISTANCE = 200      # Buffer radius used to search for an N2 coordinate on R2; measured in linear unit of dataset
+DEV_FROM_90 = 5            # Max angular deviation from 90° allowed when searching for an N2 coordinate on R2; measured in degrees
+
