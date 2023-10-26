2b7bdf30e82fa4f986bd188b3e51e38f5c287966
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-10-26 11:56:35 -0500

    removed config from __init__.py

--- scrollstats/__init__.py
+++ scrollstats/__init__.py
@@ -7,5 +7,4 @@ This file is responsible for pulling the functions out of the modules
 from .ridge_metrics import *
 from .utils import *
 from .transecting import *
-from .config import *
 from .delineation import *
