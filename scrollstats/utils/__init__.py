d089f8d0b55eb47987e2737651d971398feca43b
Andrew Vanderheiden <andrewloyd19@gmail.com>
2023-10-26 11:52:04 -0500

    removed sql_utils from __init__.py

--- scrollstats/utils/__init__.py
+++ scrollstats/utils/__init__.py
@@ -1,10 +1,8 @@
 from .geom_utils import densify_line, transform_coords, calc_dist, meanfilt, calc_cubic_spline, explode
-from .sql_utils import BendDataset
 
 __all__ = ["densify_line", 
            "transform_coords", 
            "calc_dist",
            "meanfilt",
            "calc_cubic_spline",
-           "explode",
-           "BendDataset"]
+           "explode"]
