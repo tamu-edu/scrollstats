ecbd6828d861af29543fd4347d2e9fc1f2674e41
Andrew Vanderheiden <andrewloyd19@gmail.com>
2024-04-27 15:59:50 -0500

    first unit test

---
+++ tests.py
@@ -0,0 +1,29 @@
+# Testing suite for ScrollStats
+
+from pathlib import Path
+import geopandas as gpd
+from scrollstats import LineSmoother
+
+
+RIDGES_PATH = Path("example_data/input/LBR_025_ridges_manual.geojson")
+
+
+def check_line_smoother_density():
+    """Ensure that LineSmoother generates LineStrings with a sufficient point density"""
+
+    manual_ridges = gpd.read_file("data/LBR_025_ridges_manual.geojson")
+
+    spacing = 1
+    window = 5
+    ls = LineSmoother(manual_ridges, spacing=spacing, window=window)
+    smooth_ridges = ls.execute()
+
+    tolerance = 0.01
+    deviance = smooth_ridges.geometry.apply(
+        lambda x: abs((len(x.coords) / x.length) - spacing)
+    )
+    assert all(deviance < tolerance) 
+
+
+if __name__ == "__main__":
+    check_line_smoother_density()
