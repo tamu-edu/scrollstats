c6dab87391dc4804c631c0fbc715ce8396c1f7a1
Andrew Vanderheiden <andrewloyd19@gmail.com>
2024-04-27 22:41:36 -0500

    added rt and binclass tests

--- tests.py
+++ tests.py
@@ -1,17 +1,25 @@
 # Testing suite for ScrollStats
 
 from pathlib import Path
+import numpy as np
 import geopandas as gpd
-from scrollstats import LineSmoother
+import rasterio
+from scrollstats import CalcResidualTopography, BinaryClassifier, RasterAgreementAssessor, RasterClipper, RasterDenoiser, LineSmoother, create_transects, calculate_ridge_metrics
 
 
-RIDGES_PATH = Path("example_data/input/LBR_025_ridges_manual.geojson")
 
+# Data Paths
+DEM_PATH = Path(f"example_data/input/LBR_025_dem.tif")
+BEND_PATH = Path(f"example_data/input/LBR_025_bend.geojson")
+PACKET_PATH = Path(f"example_data/input/LBR_025_packets.geojson")
+CENTERLINE_PATH = Path(f"example_data/input/LBR_025_cl.geojson")
+MANUAL_RIDGE_PATH = Path(f"example_data/input/LBR_025_ridges_manual.geojson")
+OUTPUT_DIR = Path("example_data/output")
 
 def check_line_smoother_density():
     """Ensure that LineSmoother generates LineStrings with a sufficient point density"""
 
-    manual_ridges = gpd.read_file(RIDGES_PATH)
+    manual_ridges = gpd.read_file(MANUAL_RIDGE_PATH)
 
     spacing = 1
     window = 5
@@ -25,5 +33,31 @@ def check_line_smoother_density():
     assert all(deviance < tolerance) 
 
 
+def check_rt_transformation():
+    """Check that the residual topography raster only contains values above and below zero"""
+    rt = CalcResidualTopography(DEM_PATH, 45, OUTPUT_DIR)
+    rt_path = rt.execute()
+
+    with rasterio.open(rt_path) as src:
+        array = src.read(1)
+        assert (any(array > 0) & any(array < 0))
+
+
+def check_binary_classification():
+    """Check that the binary raster only contains 1, 0, and nan around the perimeter"""
+    rt = CalcResidualTopography(DEM_PATH, 45, OUTPUT_DIR)
+    rt_path = rt.execute()
+
+    binclass = BinaryClassifier(rt_path, 0, OUTPUT_DIR)
+    binclass_path = binclass.execute()
+
+    with rasterio.open(binclass_path) as src:
+        array = src.read(1)
+        assert all(np.unique(array[~np.isnan(array)]) == np.array([0., 1.]))
+
+
+
 if __name__ == "__main__":
     check_line_smoother_density()
+    check_rt_transformation()
+    check_binary_classification()
