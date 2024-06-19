9e5366ad19b3d39920e3bd46ed5880048fe8cefe
Andrew Vanderheiden <andrewloyd19@gmail.com>
2024-06-19 00:38:13 -0500

    deleted unused utils directory

--- scrollstats/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .geom_utils import densify_line
-from .geom_utils import transform_coords
-from .geom_utils import calc_dist
-from .geom_utils import meanfilt
-from .geom_utils import calc_cubic_spline
-from .geom_utils import explode
