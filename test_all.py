fc0acfc419f87f7752bd1cfbce86b4f932e6033f
Andrew Vanderheiden <andrewloyd19@gmail.com>
2024-10-20 11:50:34 -0700

    MockRidgeData.generate_raster() now takes an array as input

--- test_all.py
+++ test_all.py
@@ -30,42 +30,6 @@ CENTERLINE_PATH = Path(f"example_data/input/LBR_025_cl.geojson")
 MANUAL_RIDGE_PATH = Path(f"example_data/input/LBR_025_ridges_manual.geojson")
 
 
-# def generate_waves(wavelength:int=30, amp:int=1, vert:int=5, signal_length:int = 60) -> np.ndarray:
-#     """
-#     Generate a 2D cosine wave with known properties for testing
-#     """
-
-#     p = 2*np.pi / wavelength
-#     x = np.arange(signal_length)
-    
-#     dem_1d = amp*np.cos(p*x) + vert
-
-#     dem_2d = np.multiply(np.ones((signal_length, signal_length)), dem_1d)
-    
-#     return dem_2d
-
-# def generate_ridges(y:int=30, rep:int=5, crs:str="EPSG:32139"):
-#     """Generate mock ridge lines with known spacing for testing """
-
-#     r = {
-#         "ridge_id": [f"r_{i:03d}" for i in range(0, rep-1)],
-#         "bend_id": ["LBR_999" for i in range(0, rep-1)],
-#         "deposit_year": [np.nan for i in range(0, rep-1)],
-#         "geometry": [LineString([[i, 0], [i, y*5]]) for i in range(y, y*5, y)]
-#     }
-#     return gpd.GeoDataFrame(data=r, geometry="geometry", crs=crs)
-
-
-# def generate_transects(y:int=30, rep:int=5, crs:str="EPSG:32139"):
-#     """Generate mock transects with known spacing for testing """
-
-#     t = {
-#         "transect_id":[f"t_{i:03d}" for i in range(0, rep-1)],
-#         "bend_id" :["LBR_999" for i in range(0, rep-1)],
-#         "geometry" : [LineString([[j, i] for j in range(0, y*rep, y)]) for i in range(y, y*rep, y)]
-#     }
-#     return gpd.GeoDataFrame(data=t, geometry="geometry", crs=crs)
-
 class MockRidgeData:
     def __init__(self,
                  wavelength:int=30,
@@ -73,7 +37,6 @@ class MockRidgeData:
                  vert_adj:int=5,
                  reps:int=5,
                  crs:str="EPSG:32139",
-                 no_data = np.nan
                  ) -> None:
         
         self.wavelength = wavelength
@@ -81,7 +44,6 @@ class MockRidgeData:
         self.vert_adj = vert_adj
         self.reps = reps
         self.crs = crs
-        self.no_data = no_data
 
 
     def generate_waves(self) -> np.ndarray:
@@ -123,22 +85,20 @@ class MockRidgeData:
         }
         return gpd.GeoDataFrame(data=t, geometry="geometry", crs=self.crs)
     
-
-    def generate_raster(self):
-        """Create a rasterio.DatasetReader object from the output of self.generate_waves()"""
-        
-        dem = self.generate_waves()
-
+    
+    def generate_raster(self, array, no_data=None):
+        """Create a rasterio.DatasetReader object from a 2D array"""
+    
         # Write DEM to disk
         with tempfile.NamedTemporaryFile(suffix=".tiff") as fp:
             with rasterio.open(fp.name, "w", driver="GTiff", 
-                            width=dem.shape[1], height=dem.shape[0], count=1, 
-                            dtype=dem.dtype, crs=self.crs, nodata=self.no_data) as dst:
-                dst.write(dem, 1)
+                            width=array.shape[1], height=array.shape[0], count=1, 
+                            dtype=array.dtype, crs=self.crs, nodata=no_data) as dst:
+                dst.write(array, 1)
 
-            dem_ds = rasterio.open(fp.name)
+            out_ds = rasterio.open(fp.name)
         
-        return dem_ds
+        return out_ds
 
 
 def test_line_smoother_density():
@@ -161,20 +121,15 @@ def test_line_smoother_density():
 def test_quadratic_profile_curvature():
     """Check that the profile curvature transformation accurately identifies ridge areas"""
     # Generate 2D cosine waves with known properties
-    wavelength = 30
-    amp = 1
-    vert = 5  # vertical adjustment for wave from the x-axis (inflection point)
-    dem = generate_waves(wavelength=wavelength, 
-                         amp=amp,
-                         vert=vert,
-                         signal_length=wavelength*2)
+    data = MockRidgeData()
+    dem = data.generate_waves()
     
     # Apply profile curvature to assess landscape convexity
-    profc = quadratic_profile_curvature(dem, window=int(wavelength/2))
+    profc = quadratic_profile_curvature(dem, window=int(data.wavelength/2))
 
     # Select areas in the generated waves known to be ridges
-    known_ridge_area = dem > vert
-    known_swale_area = dem < vert
+    known_ridge_area = dem > data.vert_adj
+    known_swale_area = dem < data.vert_adj
 
     # Assess if the known ridge/swale areas are +/-
     accurate_ridges = profc[known_ridge_area] > 0
@@ -187,21 +142,16 @@ def test_quadratic_profile_curvature():
 def test_residual_topography():
     """Check that the residual topography transformation accurately identifies ridge areas"""
     # Generate 2D cosine waves with known properties
-    wavelength = 30
-    amp = 1
-    vert = 5  # vertical adjustment for wave from the x-axis (inflection point)
-    dem = generate_waves(wavelength=wavelength, 
-                         amp=amp,
-                         vert=vert,
-                         signal_length=wavelength*2)
-    
+    data = MockRidgeData()
+    dem = data.generate_waves()
+
     # Apply residual topography to assess landscape prominence
-    rt = residual_topography(dem, w=int(wavelength/2))
+    rt = residual_topography(dem, w=int(data.wavelength/2))
     rt_not_nan = ~np.isnan(rt)
 
     # Select areas in the generated waves known to be ridges
-    known_ridge_area = dem > vert
-    known_swale_area = dem < vert
+    known_ridge_area = dem > data.vert_adj
+    known_swale_area = dem < data.vert_adj
 
     # Assess if the known ridge/swale areas are +/- (excluding nan areas)
     accurate_ridges = rt[known_ridge_area & rt_not_nan] > 0
@@ -230,37 +180,26 @@ def test_create_ridge_area_raster():
         2. the output binary raster accurately identifies ridge areas
         3. the output dem raster window has shrunk as a result of the clip and if all values outside to the geometry are cast to the no data value 
     """
-    wavelength = 30 # wavelength of cosine wave
-    amp = 1 # amplitude of cosine wave
-    vert = 5  # vertical adjustment of the cosine wave from x axis
-    rep = 5  # repetitions of the cosine wave
-    
-    no_data_value = np.nan  # no data value set for input and output rasters
 
-    # Mock DEM
-    dem = generate_waves(wavelength, amp, vert, wavelength*rep)
+    # Generate 2D cosine waves with known properties
+    data = MockRidgeData()
+    dem = data.generate_waves()
+    no_data_value = np.nan
+    dem_ds = data.generate_raster(dem, no_data=no_data_value)
 
     # Mock the bend area polygon by creating a circle in the middle of the area
     point = Point([i//2 for i in dem.shape])
-    circle = point.buffer((wavelength/2)*(rep-1))
+    circle = point.buffer((data.wavelength/2)*(data.reps-1))
 
-    # Write DEM to disk
-    with tempfile.NamedTemporaryFile(suffix=".tiff") as fp:
-        with rasterio.open(fp.name, "w", driver="GTiff", 
-                        width=dem.shape[1], height=dem.shape[0], count=1, 
-                        dtype=dem.dtype, crs="EPSG:32139", nodata=no_data_value) as dst:
-            dst.write(dem, 1)
-
-        dem_ds = rasterio.open(fp.name)
-        
+    # Identify ridge areas within the mocked dem
     binary_clip, dem_clip, binary_meta = create_ridge_area_raster(
         dem_ds = dem_ds, 
         geometry = circle, 
         no_data_value = no_data_value,
-        window=int(wavelength/2), dx=1, small_feats_size=1)
+        window=int(data.wavelength/2), dx=1, small_feats_size=1)
         
 
-    known_ridge_areas = dem_clip > vert
+    known_ridge_areas = dem_clip > data.vert_adj
     no_data_area = (binary_clip != 0) & (binary_clip!=1)
 
     # Do all classified ridge pixels correspond to known ridge pixels?
@@ -298,122 +237,86 @@ def test_create_transects():
 def test_ridge_data_extractor():
     """Test that the RidgeDataExtractor calculates expected ridge width, amplitude, and spacing"""
     
-    # Generate a 1D DEM signal with 30m peak-to-peak distance (wavelength)
-    p2p = 30
-    vert = 5
-    amp = 1
-    signal_length = p2p*2
-    dem = generate_waves(wavelength=p2p, vert=vert, signal_length=signal_length)[0]
+    # Generate a 1D cosine wave with known properties
+    data = MockRidgeData()
+    dem = data.generate_waves()[0]
+    transects = data.generate_transects()
+    ridges = data.generate_ridges()
 
-    # Create a straight transect at position 1
-    transect_substring = LineString([[0*p2p, 50], [1*p2p, 50], [2*p2p, 50]])
+    # RidgeDataExtractor calculates ridge metrics at the intersection of a single transect_substring and the ridge it intersects 
+    ## Take the first 3 verticies of the first transect to form the transect_substring 
+    transect_substring = LineString(transects.loc[0, "geometry"].coords[:3])
     position = 1
+
+    ## Take the first ridge as the intersecting ridge
+    ridge = ridges.loc[[0]]
 
     # Create a binary wave from the DEM where swales are 0 and ridges are 1
     bw = dem.copy()
-    bw[bw <= vert] = 0
-    bw[bw > vert] = 1
-
-    # Mock a ridge line that intersects the middle vertex of the transect substring
-    ridge_vals = {
-        "ridge_id": ["r_999"],
-        "bend_id": ["LBR_999"],
-        "deposit_year": [np.nan],
-        "geometry": [LineString([[30, 80], [30, 50], [30, 20]])] 
-    }
-    ridge = gpd.GeoDataFrame(data=ridge_vals)
+    bw[bw <= data.vert_adj] = 0
+    bw[bw > data.vert_adj] = 1
 
     # Calculate the three core metrics (ridge amplitude, width, and spacing) at the transect-ridge intersection
     rde_data = RidgeDataExtractor(transect_substring, position, ridge, dem, bw).dump_data()
 
-    assert rde_data["ridge_amp"] == amp*2
-    assert rde_data["ridge_width"] == p2p / 2
-    assert rde_data["pre_mig_dist"] == p2p
+    assert rde_data["ridge_amp"] == data.amp*2
+    assert rde_data["ridge_width"] == data.wavelength / 2
+    assert rde_data["pre_mig_dist"] == data.wavelength
 
 
 def test_transect_data_extractor():
     """
     Test that the TransectDataExtractor calculates expected ridge width, amplitude, and spacing for an entire transect
     
-    Mock ridges and transects are perpendicular to each other on a 30m grid.
-    Mock DEM is a simple cosine wave with a 30m wavelength.
+    Mock DEM is a simple cosine wave with a given wavelength.
+    Mock ridges and transects are perpendicular to each other on a grid with a regular spacing equal to the mock DEM's wavelength.
     """
 
-    y = 30 # wavelength of cosine wave
-    amp = 1 # amplitude of cosine wave
-    vert = 5  # vertical adjustment of the cosine wave from x axis
-    rep = 5  # repetitions of the cosine wave
-
-    # Mock DEM
-    dem = generate_waves(y, amp, 5, y*rep)
-
-    # Mock ridges
-    ridges = generate_ridges(y, rep)
-
-    # Mock transects
-    transects = generate_transects(y, rep)
+    data = MockRidgeData()
+    dem = data.generate_waves()
+    transects = data.generate_transects()
+    ridges = data.generate_ridges()
 
     # Calculate ridge metrics for the first transect
     tde = TransectDataExtractor(
         **transects.loc[0, ["transect_id", "geometry"]], 
         dem_signal=dem[0], 
-        bin_signal=(dem[0] > vert).astype(int),
+        bin_signal=(dem[0] > data.vert_adj).astype(int),
         ridges=ridges
     )
 
     transect_metrics = tde.calc_ridge_metrics()
 
-    assert all(transect_metrics["ridge_amp"] == amp*2)
-    assert all(transect_metrics["ridge_width"] == y / 2)
-    assert all(transect_metrics["pre_mig_dist"] == y)
+    assert all(transect_metrics["ridge_amp"] == data.amp*2)
+    assert all(transect_metrics["ridge_width"] == data.wavelength / 2)
+    assert all(transect_metrics["pre_mig_dist"] == data.wavelength)
 
 
 def test_bend_data_extractor():
     """
     Test that the BendDataExtractor calculates expected ridge width, amplitude, and spacing for an entire bend.
     
-    Mock ridges and transects are perpendicular to each other on a 30m grid.
-    Mock DEM is a simple cosine wave with a 30m wavelength.
+    Mock DEM is a simple cosine wave with a given wavelength.
+    Mock ridges and transects are perpendicular to each other on a grid with a regular spacing equal to the mock DEM's wavelength.
     Mock Binary Raster is a binary classification of DEM where 1s are values greater than a threshold and 0s are less than.
     Both DEM and Binary Raster are converted into temporary rasterio Dataset objects as required by BendDataExtractor
     """
-        
-    y = 30 # wavelength of cosine wave
-    amp = 1 # amplitude of cosine wave
-    vert = 5  # vertical adjustment of the cosine wave from x axis
-    rep = 5  # repetitions of the cosine wave
 
-    # Mock DEM and Binary Raster
-    dem = generate_waves(y, amp, vert, y*rep)
-    bin_arr = (dem > vert).astype(int)
+    # Generate mock data
+    data = MockRidgeData()
+    dem = data.generate_waves()
+    transects = data.generate_transects()
+    ridges = data.generate_ridges()
 
-    # Write DEM to disk
-    with tempfile.NamedTemporaryFile(suffix=".tiff") as fp:
-        with rasterio.open(fp.name, "w", driver="GTiff", 
-                        width=dem.shape[1], height=dem.shape[0], count=1, 
-                        dtype=dem.dtype, crs="EPSG:32139") as dst:
-            dst.write(dem, 1)
+    bin_arr = (dem > data.vert_adj).astype(int)
 
-            dem_ras = rasterio.open(fp.name)
-
-    # Write Binary Raster to disk
-    with tempfile.NamedTemporaryFile(suffix=".tiff") as fp:
-        with rasterio.open(fp.name, "w", driver="GTiff", 
-                        width=bin_arr.shape[1], height=bin_arr.shape[0], count=1, 
-                        dtype=bin_arr.dtype, crs="EPSG:32139") as dst:
-            dst.write(bin_arr, 1)
-        
-            bin_ras = rasterio.open(fp.name)
-
-    # Mock ridges
-    ridges = generate_ridges(y, rep)
-
-    # Mock transects
-    transects = generate_transects(y, rep)
+    # Create rasterio.DatasetReader objects from arrays
+    dem_ras = data.generate_raster(dem, no_data=np.nan)
+    bin_ras = data.generate_raster(bin_arr)
 
     # Create BendDataExtractor to calculate metrics
     bde = BendDataExtractor(transects, bin_ras, dem_ras, ridges)
 
-    assert all(bde.itx_metrics["ridge_amp"] == amp*2)
-    assert all(bde.itx_metrics["ridge_width"] == y / 2)
-    assert all(bde.itx_metrics["pre_mig_dist"] == y)
+    assert all(bde.itx_metrics["ridge_amp"] == data.amp*2)
+    assert all(bde.itx_metrics["ridge_width"] == data.wavelength / 2)
+    assert all(bde.itx_metrics["pre_mig_dist"] == data.wavelength)
