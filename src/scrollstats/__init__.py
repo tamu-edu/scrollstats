669f9dd4d7a752e9803bcee876d1671e401cfe8b
Andrew Vanderheiden <andrewloyd19@gmail.com>
2025-04-17 22:15:15 -0700

    added new files and directories from sp cookiecutter

---
+++ src/scrollstats/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2025 Andrew Vanderheiden. All rights reserved.
+
+scrollstats: An open-source python library to calculate and extract morphometrics from scroll bar floodplains
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
