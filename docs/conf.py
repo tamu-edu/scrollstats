90142f9dea47dc234e80e239899dd3f0c4de6152
Andrew Vanderheiden <andrewloyd19@gmail.com>
2025-04-20 17:15:26 -0700

    added nbsphinx configurations to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
     "sphinx_copybutton",
+    "nbsphinx",
 ]
 
 source_suffix = [".rst", ".md"]
@@ -62,3 +63,12 @@ nitpick_ignore = [
 ]
 
 always_document_param_types = True
+
+nbsphinx_execute = "auto"
+
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'png2x'}",
+    "--InlineBackend.rc=figure.dpi=96",
+]
+
+nbsphinx_kernel_name = "python3"
